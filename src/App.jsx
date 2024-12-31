a14dfe031c2fda013afd660ee44c913a49f47cd5
janada jayathilaka <86776703+JanadaJayathilaka@users.noreply.github.com>
2024-12-31 06:53:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,142 @@
+import { Fragment, useState } from "react";
+import "./App.css";
+import { Unit } from "./Components/Unit";
+import Header from "./Components/Header";
+const App = () => {
+  const [myData, setMyData] = useState([]);
+
+  const [inputData, setInputData] = useState({
+    imageUrl: "",
+    name: "",
+    city: "",
+    position: "",
+  });
+
+  console.log(myData);
+  return (
+    <Fragment>
+      <Header />
+      <div className="main-container">
+        <div className="main-left">
+          <input
+            placeholder="ImageUrl"
+            type="text"
+            value={inputData.imageUrl}
+            onChange={(e) => {
+              e.preventDefault();
+              setInputData((preInputData) => ({
+                ...preInputData,
+                imageUrl: e.target.value,
+              }));
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Name"
+            value={inputData.name}
+            onChange={(e) => {
+              e.preventDefault();
+              setInputData((preInputData) => ({
+                ...preInputData,
+                name: e.target.value,
+              }));
+            }}
+          />
+
+          <input
+            type="text"
+            placeholder="city"
+            value={inputData.city}
+            onChange={(e) => {
+              e.preventDefault();
+              setInputData((preInputData) => ({
+                ...preInputData,
+                city: e.target.value,
+              }));
+            }}
+          />
+          <input
+            type="text"
+            placeholder="position"
+            value={inputData.position}
+            onChange={(e) => {
+              e.preventDefault();
+              setInputData((preInputData) => ({
+                ...preInputData,
+                position: e.target.value,
+              }));
+            }}
+          />
+          <button
+            onClick={() => {
+              setMyData((pre) => {
+                return [
+                  ...pre,
+                  {
+                    image: inputData.imageUrl,
+                    name: inputData.name,
+                    city: inputData.city,
+                    position: inputData.position,
+                  },
+                ];
+              });
+
+              setInputData((pre) => {
+                if (pre.imageUrl.length > 0) {
+                  return {
+                    ...pre,
+                    imageUrl: "",
+                  };
+                } else {
+                  return pre;
+                }
+              });
+
+              setInputData((pre) => {
+                if (pre.name.length > 0) {
+                  return {
+                    ...pre,
+                    name: "",
+                  };
+                } else {
+                  return pre;
+                }
+              });
+
+              setInputData((pre) =>
+                pre.city.length > 0
+                  ? {
+                      ...pre,
+                      city: "",
+                    }
+                  : pre
+              );
+              setInputData((pre) =>
+                pre.position.length > 0
+                  ? {
+                      ...pre,
+                      position: "",
+                    }
+                  : pre
+              );
+            }}
+          >
+            Submit
+          </button>
+        </div>
+        <div className="main-right">
+          {myData?.map(({ image, name, city, position }, index) => (
+            <Unit
+              image={image}
+              name={name}
+              city={city}
+              position={position}
+              key={index}
+            />
+          ))}
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => (
+  <header className="header-container">
+    <h1>
+      Janada <span>Jayathilaka</span>
+    </h1>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Project</li>
+      </ul>
+    </nav>
+  </header>
+);
+export default Header;
