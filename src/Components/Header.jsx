48e54968e5b78ae9aeefe2a05f5134872bfa3f3c
janada jayathilaka <86776703+JanadaJayathilaka@users.noreply.github.com>
2024-12-31 07:45:10 +0530

    create the nav bar and styled

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import close from "../assets/close.svg";
+import menu from "../assets/menu.svg";
+const Header = () => {
+  const [menuClick, setMenuClick] = useState(false);
 
-const Header = () => (
-  <header className="header-container">
-    <h1>
-      Janada <span>Jayathilaka</span>
-    </h1>
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Project</li>
-      </ul>
-    </nav>
-  </header>
-);
+  return (
+    <header className="header-container">
+      <h1>
+        Janada <span>Jayathilaka</span>
+      </h1>
+      <nav>
+        <img
+          src={menu}
+          alt="menu"
+          onClick={() => {
+            setMenuClick(true);
+          }}
+        />
+        <ul
+          style={{
+            right: `${menuClick ? "-20px" : "-250px"}`,
+          }}
+        >
+          <div
+            className="header-menuclose"
+            onClick={() => {
+              setMenuClick(false);
+            }}
+          >
+            <img src={close} alt="close icon" />
+          </div>
+          <div className="header-menu">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Project</li>
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 export default Header;
